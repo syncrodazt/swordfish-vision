ba15766c0d19f807ba8e642bdfabc2cd9b2b73b4
Thayakorn Rakwetpakorn <t.rakwetpakorn@outlook.com>
2022-12-16 16:45:15 +0900

    Add function to save position data to file

--- movdetect.py
+++ movdetect.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-vid = cv.VideoCapture('swordfish 02.mov')
+vid = cv.VideoCapture('swordfish 01.mov')
 height = vid.get(cv.CAP_PROP_FRAME_HEIGHT)  # always 0 in Linux python3
 width = vid.get(cv.CAP_PROP_FRAME_WIDTH)  # always 0 in Linux python3
 print("opencv: height:{} width:{}".format(height, width))
@@ -8,17 +8,19 @@ print("opencv: height:{} width:{}".format(height, width))
 cv.namedWindow('Swordfish Tail Tracking', cv.WINDOW_NORMAL)
 # cv.resizeWindow("Swordfish Tail Tracking", 1080, 1920)
 count = 0
+pos_str = ""
 while vid.isOpened():
     ret, im = vid.read()
     if not ret:
         continue
     # edges = cv.Canny(frame, 50, 500)
     im = cv.rotate(im, cv.ROTATE_180)
-    y = 0
-    x = 50
-    h = 850
-    w = 720
-    im = im[y: (y + h), x: (x + w)]
+
+    # y = 0
+    # x = 50
+    # h = 850
+    # w = 720
+    # im = im[y: (y + h), x: (x + w)]
 
     im_contour = im.copy()
     imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
@@ -36,6 +38,7 @@ while vid.isOpened():
         temp = xnp[maxindex[1]]
         # print(temp)
         maxs.append(temp)
+        pos_str = pos_str+f"{temp[0]}, {temp[1]}"+"\n"
         # cv.circle(im, temp, radius=1, color=(0, 0, 255), thickness=5)
     maxsnp = np.array(maxs)
     maxindex = np.argmax(maxsnp, axis=0)
@@ -51,7 +54,10 @@ while vid.isOpened():
     #cv.imwrite("frame%d.jpg" % count, frame)
     count = count + 1
     if cv.waitKey(10) & 0xFF == ord('q'):
+        with open('time01.csv', 'w') as f:
+            f.write(pos_str)
         break
+
 
 vid.release()
 cv.destroyAllWindows()
